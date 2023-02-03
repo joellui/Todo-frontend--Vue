cd8eb951a356e97de25b8608c4afbbc23cf7dfbc
Joel Louis <joellouis2000@gmail.com>
2023-02-03 17:23:45 +0530

    base application made

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,45 @@
 <template>
-  <div id="app">
-    <ToDos v-bind:todoEntries="todoEntries"/>
-    <AddToDoButton @add-todo-event="addToDoItem"/>
+  <div>
+    <h1>Todo List</h1>
+    <form @submit.prevent="addTodo">
+      <input type="text" v-model="newTodo" placeholder="Add Todo">
+      <button type="submit">Add</button>
+    </form>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        <input type="checkbox" v-model="todo.completed" @change="markTodoComplete(todo)">
+        <span :class="{'completed': todo.completed}">{{ todo.text }}</span>
+        <button @click="removeTodo(index)">X</button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-  import ToDos from './components/ToDos'
-  import AddToDoButton from './components/AddToDoButton.vue'
-  export default {
-  name: 'App',
-  components: {
-  ToDos,
-  AddToDoButton
-  },
+export default {
   data() {
-      return {
-          todoEntries: [
-              {
-                  id: 1,
-                  title: 'Infos sammeln',
-                  completed: false
-              },
-              {
-                  id: 2,
-                  title: 'Test Projekt erstellen',
-                  completed: false
-              },
-              {
-                  id: 3,
-                  title: 'Blog Beitrag schreiben',
-                  completed: false
-              },
-              ],
-          }
-      },
-      methods: {
-    addToDoItem(newToDoItem) {
-      this.todoEntries = [...this.todoEntries, newToDoItem];
-    }
+    return {
+      newTodo: '',
+      todos: []
+    };
   },
+  methods: {
+    addTodo() {
+      this.todos.push({ text: this.newTodo, completed: false });
+      this.newTodo = '';
+    },
+    markTodoComplete(todo) {
+      todo.completed = !this.todo.completed;
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    }
   }
-  
+};
 </script>
+
+<style>
+.completed {
+  text-decoration: line-through;
+}
+</style>
