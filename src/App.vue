4cf1737f00624b5dcc87b033ce3db01757da3b39
Joel Louis <joellouis2000@gmail.com>
2023-02-08 20:46:21 +0530

    Major update: - added functionaly to login -created navbar - connected to backend django server

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <NavBar @userStatus="handleuserStatus" />
+  <NavBar @userStatus="handleUserStatus" @initTodoList="handleUpdateList" />
   <div>
     <h1>Todo List</h1>
     <form @submit.prevent="addTodo">
@@ -9,14 +9,15 @@
     <ul>
       <li v-for="(todo, index) in todos" :key="index">
         <input type="checkbox" v-model="todo.completed" @change="markTodoComplete(todo)">
-        <span :class="{'completed': todo.completed}">{{ todo.text }}</span>
-        <button @click="removeTodo(index)">X</button>
+        <span :style="{ textDecoration: todo.completed ? 'line-through' : 'none' }">{{ todo.task }}</span>
+        <button @click="removeTodo(todo,index)">X</button>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import api from "./api";
 import NavBar from "./components/NavBar";
 
 export default {
@@ -31,22 +32,60 @@ export default {
     };
   },
   methods: {
-    addTodo() {
-      this.todos.push({ text: this.newTodo, completed: false });
-      this.newTodo = '';
+    async getTodos(){
+      try{
+        const response = await api.get('/todos/task-list/')
+        console.log(response.data)
+      }catch(err){
+        console.error(err)
+      }
     },
-    markTodoComplete(todo) {
-      todo.completed = !this.todo.completed;
+
+    handleUserStatus(data){
+      console.log(data)
+      return data
     },
-    removeTodo(index) {
+    handleUpdateList(data){
+      this.todos = data
+      console.log(data)
+      return data
+    },
+
+    async addTodo() {
+      const newTodo = { task: this.newTodo, completed: false };
+      this.todos.push(newTodo);
+      try{
+        await api.post("/todos/task-create", newTodo)
+        this.getTodos()
+      }catch(err){
+        console.error(err)
+      }
+    },
+
+    async markTodoComplete(todo) {
+      console.log(todo)
+      const newTodo = { task: todo.task, completed: todo.completed };
+
+      try{
+        await api.put(`/todos/task-update/${todo.id}`, newTodo)
+        // this.getTodos()
+      }catch(err){
+        console.error(err)
+      }
+
+    },
+
+    async removeTodo(todo, index) {
       this.todos.splice(index, 1);
+      console.log(todo)
+      try{
+        await api.delete(`/todos/task-delete/${todo.id}`)
+        this.getTodos()
+      }catch(err){
+        console.error(err)
+      }
+      
     }
   }
 };
 </script>
-
-<style>
-.completed {
-  text-decoration: line-through;
-}
-</style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,7 +7,7 @@
         <button style="margin: 3px;" @click="showRegister = !showRegister">Register</button>
       </div>
       <div v-else>
-        <button @click="logout">Logout</button>
+        <button @click="logout" >Logout</button>
       </div>
     </nav>
 
@@ -50,7 +50,7 @@
 </template>
 
 <script>
-import axios from "axios";
+import api from "@/api";
 export default {
 
   data() {
@@ -63,22 +63,31 @@ export default {
   methods: {
     async login() {
 
-      const url = 'http://127.0.0.1:8000/user/login/'
+      const url = '/user/login/'
       const auth = {
           username: this.usernameLogin,
           password: this.passwordLogin
         }
 
       try{
-        const response = await axios.post(url, auth);
+        const response = await api.post(url, auth);
         if (response.status == 200){
           this.loggedIn = true
           this.$emit('userStatus',{'logStatus': this.loggedIn,'token': response.data.token})
           alert(response.data.token)
+
+          localStorage.setItem('token', response.data.token);
+          api.defaults.headers.common['Authorization'] = `token ${response.data.token}`;
+
         }else{
           alert(response.status)
           console.log(response.data)
         }
+
+        // initialize todo list for the database
+        const res = await api.get('/todos/task-list/')
+        this.$emit('initTodoList',res.data)
+        // console.log(res.data)
 
       }catch(error){
         console.error(error)
@@ -87,7 +96,7 @@ export default {
     },
 
     async register() {
-      const url = 'http://127.0.0.1:8000/user/register/'
+      const url = '/user/register/'
       const data = {
         "username": this.usernameRegister,
         "password": this.passwordRegister,
@@ -95,7 +104,7 @@ export default {
 
       
         try{
-          const res = await axios.post(url, data);
+          const res = await api.post(url, data);
           if (res.status == 201){
             alert("now login with the credentials")
           }else{
@@ -107,11 +116,18 @@ export default {
         }
     },
 
-    logout(){
+    async logout(){
       this.loggedIn = false
       this.$emit('userStatus',{'logStatus': this.loggedIn,'token': null})
+      localStorage.setItem('token', null)
+      api.defaults.headers.common['Authorization'] = null;
       alert("logout")
-      
+      try{
+      const res = await api.get('/todos/task-list/')
+      this.$emit('initTodoList',res.data)
+      }catch(err){
+        console.error(err)
+      }
     }
   }
 }
